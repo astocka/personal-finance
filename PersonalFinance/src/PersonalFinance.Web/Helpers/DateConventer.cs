67789fb3828c84e187a453ce65aee86e0c6c38ac
Aleksandra Stocka <aleksandra.stocka689@gmail.com>
2019-09-29 12:27:42 +0200

    add date conventer with unit tests

---
+++ PersonalFinance/src/PersonalFinance.Web/Helpers/DateConventer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonalFinance.Web.Helpers
+{
+    public class DateConventer
+    {
+        public string ConvertNumberToMonth(int number)
+        {
+            var months = new Dictionary<int, string>()
+            {
+                { 1, "January" }, { 2, "February" }, {3, "March" }, {4, "April" }, {5, "May" }, {6, "June" },
+                { 7, "July" }, {8, "August" }, {9, "September" }, {10, "October" }, {11, "November" }, {12, "December" }
+            };
+
+            if (number > 0 && number < 13)
+            {
+                return months[number];
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
---
+++ PersonalFinance/test/PersonalFinance.Web.Helpers.Tests/DateConventerTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonalFinance.Web.Helpers.Tests
+{
+    [TestFixture]
+    public class DateConventerTests
+    {
+        [Test]
+        public void ConvertNumberToMonthTest_TakeNumber_ReturnMonth([Range(1, 12, 1)] int month)
+        {
+            var months = new Dictionary<int, string>()
+            {
+                { 1, "January" }, { 2, "February" }, {3, "March" }, {4, "April" }, {5, "May" }, {6, "June" },
+                { 7, "July" }, {8, "August" }, {9, "September" }, {10, "October" }, {11, "November" }, {12, "December" }
+            };
+
+            var dateConventer = new DateConventer();
+
+            Assert.That(dateConventer.ConvertNumberToMonth(month), Is.EqualTo(months[month]));
+            Assert.That(dateConventer.ConvertNumberToMonth(0), Is.EqualTo(""));
+            Assert.That(dateConventer.ConvertNumberToMonth(13), Is.EqualTo(""));
+            Assert.That(dateConventer.ConvertNumberToMonth(-4), Is.EqualTo(""));
+        }
+    }
+}
